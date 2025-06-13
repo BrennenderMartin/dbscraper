f7d9b2ade10f259db5a73afda783220717d40cc4
BrennenderMartin <mattis2612@outlook.de>
2025-06-13 14:54:51 +0200

    Update README, add converter script, and enhance main functionality to process delays

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # dbscraper
 
 Datensatz vom User: https://github.com/2mal3
+
+Dies ist der Plan für das dbscraper Projekt:
+
+Zeitraum der Daten:
+    13.01.2025 bis 25.05.2025
+
+- Darstellung: 
+    - welcher Zug on av. am meisten Verspätung hat
+    - welcher Zug on av. am wenigsten Verspätung hat
+    - wo am meisten Verspätung ist
+    - wo am wenigsten Verspätung ist
+    - Graphisch auf einer Deutschlandkarte, am welchen Tagen welcher Zug von wo über wo wohin fährt
+    - Zeitgraph, in welchem Monat am meisten Verspätung war
--- src/dbscraper/main.py
+++ src/dbscraper/main.py
@@ -1,1 +1,28 @@
+import json
+
 print("Hello World!")
+
+db = json.load(open("./src/dbscraper/db.json", "r", encoding="utf-8"))
+print(f"Loaded {len(db)} tables from db.json")
+
+longest_delay = 0
+long_row = 0
+
+for table_name, table_data in db.items():
+    print(f"Table: {table_name}")
+
+    if table_name != "Stop":
+        print("Skipping table:", table_name)
+        continue
+    else:
+        print("Processing table:", table_name)
+        for row in table_data:
+            print(row)
+            for key, value in row:
+                if key == "arrival_delay" or key == "departure_delay":
+                    if value > longest_delay:
+                        longest_delay = value
+                        long_row = row
+
+
+print(longest_delay)
